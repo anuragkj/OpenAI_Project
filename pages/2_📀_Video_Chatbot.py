b7ea9a2a233554442b595c6a5777e064dc0e0035
ZeroDay <62294866+GitReboot@users.noreply.github.com>
2023-04-27 22:21:57 +0400

    formatting the code

--- pages/2_📀_Video_Chatbot.py
+++ pages/2_📀_Video_Chatbot.py
@@ -14,43 +14,41 @@ from langchain.vectorstores import FAISS
 from streamlit_chat import message
 
 load_dotenv()
-api_key = st.secrets["OPENAI_KEY"]  
+api_key = st.secrets["OPENAI_KEY"]
+
 
 # vectors = getDocEmbeds("gpt4.pdf")
 # qa = ChatVectorDBChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), vectors, return_source_documents=True)
 
 async def main():
-
     async def storeDocEmbeds(filename):
-    
-        
+
         corpus = st.session_state['stored_text']
-        
-        splitter =  RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200,)
+
+        splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, )
         chunks = splitter.split_text(corpus)
-        
-        embeddings = OpenAIEmbeddings(openai_api_key = api_key)
+
+        embeddings = OpenAIEmbeddings(openai_api_key=api_key)
         vectors = FAISS.from_texts(chunks, embeddings)
-        
+
         with open(filename + ".pkl", "wb") as f:
             pickle.dump(vectors, f)
 
-        
     async def getDocEmbeds(filename):
-        
+
         if not os.path.isfile(filename + ".pkl"):
             await storeDocEmbeds(filename)
-        
+
         with open(filename + ".pkl", "rb") as f:
             global vectores
             vectors = pickle.load(f)
-            
+
         return vectors
-    
 
     async def conversational_chat(query):
         vectors = await getDocEmbeds('random')
-        qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-4"), retriever=vectors.as_retriever(), return_source_documents=True)
+        qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-4"), retriever=vectors.as_retriever(),
+                                                   return_source_documents=True)
         result = qa({"question": query, "chat_history": st.session_state['history']})
         st.session_state['history'].append((query, result["answer"]))
         # print("Log: ")
@@ -64,13 +62,12 @@ async def main():
         if 'history' not in st.session_state:
             st.session_state['history'] = []
 
-
-        #Creating the chatbot interface
-        with st.sidebar:   
-        # Add the logo image to the sidebar
+        # Creating the chatbot interface
+        with st.sidebar:
+            # Add the logo image to the sidebar
             image = Image.open("assets/images/feynmanai-no-bg.png")
             st.image(image)
-            
+
             # Add the header to the sidebar
             st.header("Understanding complex topics made simple!")
             st.write("_Your very own personal tutor._")
@@ -78,15 +75,16 @@ async def main():
         if 'ready' not in st.session_state:
             st.session_state['ready'] = False
 
-        # uploaded_file = st.file_uploader("Choose a file (Generate report from Homepage if help required over the video)", type="pdf")
+        # uploaded_file = st.file_uploader("Choose a file (Generate report from Homepage if help required over the
+        # video)", type="pdf")
 
         # if st.button("Generate Chatbot"):
-            
-            # Add your code here that needs to be executed
-            # pdf = PyPDF2.PdfFileReader()
-            # qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), retriever=vectors.as_retriever(), return_source_documents=True)
 
-            # st.session_state['ready'] = True
+        # Add your code here that needs to be executed pdf = PyPDF2.PdfFileReader() qa =
+        # ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"),
+        # retriever=vectors.as_retriever(), return_source_documents=True)
+
+        # st.session_state['ready'] = True
 
         # st.divider()
         st.session_state['ready'] = True
@@ -106,7 +104,8 @@ async def main():
 
             with container:
                 with st.form(key='my_form', clear_on_submit=True):
-                    user_input = st.text_input("Query:", placeholder="e.g: Summarize the video in a few sentences", key='input')
+                    user_input = st.text_input("Query:", placeholder="e.g: Summarize the video in a few sentences",
+                                               key='input')
                     submit_button = st.form_submit_button(label='Send')
 
                 if submit_button and user_input:
@@ -120,11 +119,11 @@ async def main():
                         message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
                         message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
     else:
-        with st.sidebar:   
-        # Add the logo image to the sidebar
+        with st.sidebar:
+            # Add the logo image to the sidebar
             image = Image.open("assets/images/feynmanai-no-bg.png")
             st.image(image)
-            
+
             # Add the header to the sidebar
             st.header("Understanding complex topics made simple!")
             st.write("_Your very own personal tutor._")
--- pages/3_🤖_PDF_Chatbot.py
+++ pages/3_🤖_PDF_Chatbot.py
@@ -16,40 +16,38 @@ from langchain.vectorstores import FAISS
 from streamlit_chat import message
 
 load_dotenv()
-api_key = st.secrets["OPENAI_KEY"]  
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_KEY"]  
+api_key = st.secrets["OPENAI_KEY"]
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_KEY"]
+
 
 # vectors = getDocEmbeds("gpt4.pdf")
 # qa = ChatVectorDBChain.from_llm(ChatOpenAI(model_name="gpt-3.5-turbo"), vectors, return_source_documents=True)
 
 async def main():
-
     async def storeDocEmbeds(file, filename):
-    
+
         reader = PdfReader(file)
         corpus = ''.join([p.extract_text() for p in reader.pages if p.extract_text()])
-        
-        splitter =  RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200,)
+
+        splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200, )
         chunks = splitter.split_text(corpus)
-        
-        embeddings = OpenAIEmbeddings(openai_api_key = api_key)
+
+        embeddings = OpenAIEmbeddings(openai_api_key=api_key)
         vectors = FAISS.from_texts(chunks, embeddings)
-        
+
         with open(filename + ".pkl", "wb") as f:
             pickle.dump(vectors, f)
 
-        
     async def getDocEmbeds(file, filename):
-        
+
         if not os.path.isfile(filename + ".pkl"):
             await storeDocEmbeds(file, filename)
-        
+
         with open(filename + ".pkl", "rb") as f:
             global vectores
             vectors = pickle.load(f)
-            
+
         return vectors
-    
 
     async def conversational_chat(query):
         result = qa({"question": query, "chat_history": st.session_state['history_pdf']})
@@ -58,20 +56,18 @@ async def main():
         # print(st.session_state['history'])
         return result["answer"]
 
-
     llm = ChatOpenAI(model_name="gpt-4")
     chain = load_qa_chain(llm, chain_type="stuff")
 
     if 'history_pdf' not in st.session_state:
         st.session_state['history_pdf'] = []
 
-
-    #Creating the chatbot interface
-    with st.sidebar:   
-    # Add the logo image to the sidebar
+    # Creating the chatbot interface
+    with st.sidebar:
+        # Add the logo image to the sidebar
         image = Image.open("assets/images/feynmanai-no-bg.png")
         st.image(image)
-        
+
         # Add the header to the sidebar
         st.header("Understanding complex topics made simple!")
         st.write("_Your very own personal tutor._")
@@ -82,14 +78,14 @@ async def main():
     uploaded_file = st.file_uploader("Choose a file ", type="pdf")
 
     if uploaded_file is not None:
-
         with st.spinner("Processing..."):
-        # Add your code here that needs to be executed
+            # Add your code here that needs to be executed
             uploaded_file.seek(0)
             file = uploaded_file.read()
             # pdf = PyPDF2.PdfFileReader()
             vectors = await getDocEmbeds(io.BytesIO(file), uploaded_file.name)
-            qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-4"), retriever=vectors.as_retriever(), return_source_documents=True)
+            qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(model_name="gpt-4"), retriever=vectors.as_retriever(),
+                                                       return_source_documents=True)
 
         st.session_state['ready_pdf'] = True
 
@@ -98,7 +94,8 @@ async def main():
     if st.session_state['ready_pdf']:
 
         if 'generated_pdf' not in st.session_state:
-            st.session_state['generated_pdf'] = ["Welcome! You can now ask any questions regarding " + uploaded_file.name]
+            st.session_state['generated_pdf'] = [
+                "Welcome! You can now ask any questions regarding " + uploaded_file.name]
 
         if 'past_pdf' not in st.session_state:
             st.session_state['past_pdf'] = ["Hey!"]
@@ -111,7 +108,8 @@ async def main():
 
         with container:
             with st.form(key='my_form', clear_on_submit=True):
-                user_input = st.text_input("Query:", placeholder="e.g: Summarize the paper in a few sentences", key='input')
+                user_input = st.text_input("Query:", placeholder="e.g: Summarize the paper in a few sentences",
+                                           key='input')
                 submit_button = st.form_submit_button(label='Send')
 
             if submit_button and user_input:
